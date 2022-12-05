150cd5fa630f44af07e57cd644ebf86571f5e9c9
Eunice <ewasserman-lom@students.hackleyschool.org>
2022-12-05 08:59:30 -0500

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Eunice Wasserman-Lom on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
